ae10a6670a2edf4ef3a302cc3f02865e7b88825e
crystal9210 <sy9210sss@yahoo.co.jp>
2023-09-03 20:52:02 +0900

    add files

---
+++ BasicCalculations.py
@@ -0,0 +1,15 @@
+print(5*5*5*5*5)
+print(5**5)
+
+print(15/7)
+print(15//7)
+print(15%7)
+
+x=10
+y=5
+print(x*y)
+
+pie=3.14153725256
+print(pie)
+print(round(pie,3))
+# print(a)  #error
---
+++ CalcWithMathLibrary.py
@@ -0,0 +1,7 @@
+import math 
+
+a=math.sqrt(25)
+print(a)
+
+b=math.log2(100)
+print(b)
